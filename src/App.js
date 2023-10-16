9a2ee0cef7869ff3cf2b9e3218b349d6d59a7c2c
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-16 15:20:53 +0300

    darbs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [count, setCount] = useState(0);
+  return(
+    <>
+    
+    <h1>sveiki!</h1>
+    
+    <div>
+      <h2>cik lodes sodien gusitis izdzera? atbilde ir - {count} </h2>
+      <input type = 'numbers'></input>
+      <button onClick={() => setCount(count + 1)}>
+        +1
+      </button>
     </div>
-  );
+    <div>
+      
+      <button onClick={() => setCount(count - 1)}>
+        -1
+      </button>
+
+    </div>
+    
+    </>
+  )
 }
 
 export default App;
